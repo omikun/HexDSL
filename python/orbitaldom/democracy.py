b7f833a46de6d4dcd2fb52579098e341c6bf77db
Si Li <omikun@gmail.com>
2018-02-14 16:16:27 -0800

    Update democracy.py

--- python/orbitaldom/democracy.py
+++ python/orbitaldom/democracy.py
@@ -5,11 +5,11 @@ from issue import Issue, Citizen
 class Democracy:
     'a population voting on bills that affects their preferences'
     # TODO reduce number of 100% votes...
-    def __init__(self, name, pop, public=True):
+    def __init__(self, name, pop, itb=itbounds, public=True):
         self.name = name
         self.numpop = pop  # number of peeps voting for bills
         self.topics = itbounds
-        self.pop = [Citizen(itb=itbounds, public=public) for i in xrange(pop)]
+        self.pop = [Citizen(itb=itb, public=public) for i in xrange(pop)]
         self.bill = None  # dict{ program: value change, }
         self.public = public
 
@@ -41,14 +41,19 @@ class Democracy:
             self.numvoted += 1
         return didVote
 
+    def getStats(self, t):
+        l = [cit.topics[t] for cit in self.pop if t in cit.topics]
+        if l == []:
+            l = [0]
+        return sum(l)/len(l), min(l), max(l)
+
     def getPulse(self, topics=None):
         if not topics:
             topics = self.topics
         prefs = {}
+        print topics
         for t in topics:
-            tmin = min(cit.topics[t] for cit in self.pop)
-            tmax = max(cit.topics[t] for cit in self.pop)
-            tavg = sum(cit.topics[t] for cit in self.pop)/float(len(self.pop))
+            tavg, tmin, tmax = self.getStats(t)
             prefs[t] = [tavg, tmin, tmax]
         return prefs
 
@@ -58,18 +63,12 @@ class Democracy:
             self.bill = bill
         self.numvoted = 0
         votes = sum(cit.vote(self.bill) for cit in self.pop if self.countVote(cit.didvote(self.bill))) 
-        # prefs = {topic:sum(cit.topics[topic] 
-        #           for cit in self.pop)/float(len(self.pop)) 
-        #               for topic in self.bill.topics }
         prefs = self.getPulse(self.bill)
         opinion = sum(cit.opinion for cit in self.pop) / float(len(self.pop))
         print 'avg pref: ', prefs, ' avg opinion: ', opinion
-        print 'votes on bill ', votes, ' out of ', self.numvoted, ': ', votes/float(self.numvoted)
-        billPassed = None
-        if self.public:
-            billPassed = votes * 2 >= self.numvoted
-        else:
-            billPassed = votes * 2 >= self.numpop
+        totalCount = self.numvoted if self.public else self.numpop
+        print 'votes on bill ', votes, ' out of ', totalCount, ': ', votes/float(totalCount)
+        billPassed = votes * 2 >=  totalCount
         return billPassed
 
 if __name__ == '__main__':
